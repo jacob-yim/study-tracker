c00cb7d970f0939351a7822f3667f8af9adefb40
jacob-yim <57049934+jacob-yim@users.noreply.github.com>
2021-01-17 02:40:07 -0800

    added site monitoring

--- background.js
+++ background.js
@@ -1,52 +1,46 @@
+window.pages = []
 window.startTime = {}
-window.prevTime = {}
+window.storedTime = {}
 window.instances = {}
 window.idToUrl = {}
 
-chrome.tabs.onCreated.addListener(function (tab) {
-    openPage(tab.url)
-    window.idToUrl[tab.id] = tab.url
-})
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    openPage(changeInfo.url)
-    closePage(tab.url)
-    window.idToUrl[tab.id] = changeInfo.url
+    if (changeInfo.url != undefined) {
+        if (tabId in window.idToUrl) {
+            closePage(window.idToUrl[tabId])
+        }
+        let hn = new URL(changeInfo.url).hostname
+        openPage(hn)
+        window.idToUrl[tab.id] = hn
+    }
 })
 
 chrome.tabs.onRemoved.addListener(function (tabId, removeInfo) {
-    closePage()
+    if (tabId in window.idToUrl) {
+        closePage(window.idToUrl[tabId])
+    }
 })
 
 function openPage(url) {
-    if (!window.startTime.has(url)) {
-        if (window.prevTime.has(url)) {
-            window.startTime[url] = Date.now() + window.prevTime[url]
-        } else {
-            window.startTime[url] = Date.now()
-        }
+    if (!(window.pages.includes(url))) {
+        window.pages.push(url)
+        window.storedTime[url] = 0
+        window.instances[url] = 0
+        console.log(window.pages.toString())
     }
-    if (window.instances.has(url)) {
-        window.instances[url] += 1
-    } else {
-        window.instances[url] = 1
+    if (!(url in window.startTime)) {
+        window.startTime[url] = Date.now()
     }
+    window.instances[url] += 1
 }
 
 function closePage(url) {
-    if (!window.instances.has(url)) {
-        throw `No pages with url ${url} open`
+    if (!(url in window.instances) || (window.instances[url] == 0)) {
+        throw "hi"
     }
     if (window.instances[url] == 1) {
-        let diff = Date.now() - window.startTime[url]
-        if (window.prevTime.has(url)) {
-            window.prevTime[url] += diff
-        } else {
-            window.prevTime[url] = diff
-        }
-        window.startTime.delete(url)
-        window.instances.delete(url)
-    } else {
-        window.instances[url] -= 1
+        window.storedTime[url] += Date.now() - window.startTime[url]
+        delete window.startTime[url]
     }
+    window.instances[url] -= 1
 }
--- popup.js
+++ popup.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const bg = chrome.extension.getBackgroundPage()
-    Object.keys(bg.startTime).forEach(function (url) {
-        let diff = Date.now() - bg.startTime[url]
+    bg.pages.forEach(function (url) {
+        var diff = bg.storedTime[url]
+        if (url in bg.startTime) {
+            diff += Date.now() - bg.startTime[url]
+        }
         let hrs = Math.floor(diff / 3600000)
         let mins = Math.floor((diff % 3600000) / 60000)
         const div = document.createElement('div')
         div.textContent = `${url}: ${hrs} hours, ${mins} minutes`
         document.body.appendChild(div)
     })
+
 }, false)
