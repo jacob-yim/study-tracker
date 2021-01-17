eebc8fb6aa288cf977e6063b429b32723848e660
jacob-yim <57049934+jacob-yim@users.noreply.github.com>
2021-01-17 03:18:14 -0800

    added web activity sorting

--- popup.js
+++ popup.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     const bg = chrome.extension.getBackgroundPage()
-    bg.pages.forEach(function (url) {
-        var diff = bg.storedTime[url]
-        if (url in bg.startTime) {
-            diff += Date.now() - bg.startTime[url]
+    const totalTimes = {}
+    bg.pages.forEach(function (hn) {
+        let diff = bg.storedTime[hn]
+        if (hn in bg.startTime) {
+            diff += Date.now() - bg.startTime[hn]
         }
+        totalTimes[hn] = diff
+    })
+    let topPages = [...bg.pages]
+    topPages.sort(function(hn1, hn2) {
+        return totalTimes[hn2] - totalTimes[hn1]
+    })
+    topPages.slice(0, 5).forEach(function (hn) {
+        let diff = totalTimes[hn]
         let hrs = Math.floor(diff / 3600000)
         let mins = Math.floor((diff % 3600000) / 60000)
         const div = document.createElement('div')
-        div.textContent = `${url}: ${hrs} hours, ${mins} minutes`
+        div.textContent = `${hn}: ${hrs} hours, ${mins} minutes`
         document.body.appendChild(div)
     })
-
 }, false)
