33e077427a4e3bafa5ef2501c0553d0f9349ac1d
Jacob Yim <57049934+jacob-yim@users.noreply.github.com>
2021-01-17 18:23:21 -0800

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const bg = chrome.extension.getBackgroundPage()
-    function input() {
-        bg.studyLength = document.getElementById("studyLenInput").value
-        bg.breakLength = document.getElementById("breakLenInput").value
-        bg.studyTimer.start()
-    }
-})
