767a7f3c05f28fd907bd843205713d814668537f
Jacob Yim <57049934+jacob-yim@users.noreply.github.com>
2021-01-17 18:23:44 -0800

    Delete alarmbutton.js

--- alarmbutton.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const bg = chrome.extension.getBackgroundPage()
-    document.querySelector('button').addEventListener('click', function () {
-        bg.studyTimer.start()
-    }, false)
-}, false)
